9963fc0bbd1297237839d76139eb99d6849d88ba
KurnakovMaksim <maksk210@gmail.com>
2023-06-01 22:34:23 +0900

    Initial commit

---
+++ KurnakovTestNuget.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace KurnakovTestNuget.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ KurnakovTestNuget/MyConsoleLog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KurnakovTestNuget
+{
+    public static class MyConsoleLog
+    {
+        public static void Log(string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
